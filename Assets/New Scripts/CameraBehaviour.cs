76c1911416516525da6fd23c1cab4feeb254ac7c
AlecNep <anepute@live.com>
2017-03-23 14:51:13 -0400

    stuff and things

--- Assets/New Scripts/CameraBehaviour.cs
+++ Assets/New Scripts/CameraBehaviour.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class CameraBehaviour : MonoBehaviour 
 {
-     public Transform targetPosition;
-     public Transform targetRotation; //Optional of course
+     public Transform targetPositionRight;
+     public Transform targetRotationRight;
 
-     public Transform initialPosition;
+     public Transform targetPositionLeft;
+     public Transform targetRotationLeft;
+
+    //Optional of course
+
+    public Transform initialPosition;
      public Transform initialRotation;
 
      public float smoothFactor = 2;
      public bool buttonPressed;
+     public bool aimLeft;
 
 	// Use this for initialization
 	void Start () 
@@ -24,21 +30,34 @@ public class CameraBehaviour : MonoBehaviour
 	void Update () 
     {
         if (Input.GetKeyDown(KeyCode.Alpha1) || (Input.GetKeyDown(KeyCode.JoystickButton4)))
+        {
             buttonPressed = true;
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+                aimLeft = true;
+            else if (Input.GetKeyUp(KeyCode.Alpha2))
+                aimLeft = false;
+        }
+
 
         else if (Input.GetKeyUp(KeyCode.Alpha1) || (Input.GetKeyUp(KeyCode.JoystickButton4)))
             buttonPressed = false;
 
         if (buttonPressed == true) 
         { 
-                 transform.position = Vector3.Lerp(transform.position, targetPosition.position, Time.deltaTime * smoothFactor);
-                 transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation.rotation, Time.deltaTime * smoothFactor);
+                 transform.position = Vector3.Lerp(transform.position, targetPositionRight.position, Time.deltaTime * smoothFactor);
+                 transform.rotation = Quaternion.Slerp(transform.rotation, targetRotationRight.rotation, Time.deltaTime * smoothFactor);
         }
 
         else if (buttonPressed == false)
         {
             transform.position = Vector3.Lerp(transform.position, initialPosition.position, Time.deltaTime * smoothFactor);
             transform.rotation = Quaternion.Slerp(transform.rotation, initialRotation.rotation, Time.deltaTime * smoothFactor); 
+        }
+
+        else if (aimLeft == true)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetPositionLeft.position, Time.deltaTime * smoothFactor);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetPositionLeft.rotation, Time.deltaTime * smoothFactor);
         }
 		
 
